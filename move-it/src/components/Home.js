23f734aef3c12aa284fc04ede418c126d043350d
jwballentine <wallace.ballentine@gmail.com>
2019-08-25 21:50:07 -0600

    adding form files

---
+++ move-it/src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { PureComponent } from "react";
+
+import "./home.css";
+
+class Home extends PureComponent {
+  render() {
+    return (
+      <div className="home">
+        <h2>Home</h2>
+        {/* <img src={movingPic}></img> */}
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ move-it/src/components/Moving.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+
+import { Form, Icon, Input, Button, DatePicker } from "antd";
+
+import "./moving.css";
+
+class Moving extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+        this.setState(values);
+      }
+    });
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+
+    const config = {
+      rules: [
+        { type: "object", required: true, message: "Please select time!" }
+      ]
+    };
+
+    return (
+      <div className="moving-form">
+        <Form
+          onSubmit={this.handleSubmit}
+          className="login-form"
+          layout="horizontal"
+        >
+          <Form.Item>
+            {getFieldDecorator("date-picker", config)(<DatePicker />)}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator("email", {
+              rules: [{ required: true, message: "Please input your email!" }]
+            })(
+              <Input
+                prefix={
+                  <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                }
+                placeholder="Email"
+              />
+            )}
+          </Form.Item>
+          Moving From:
+          <Form.Item>
+            {getFieldDecorator("streetfrom", {
+              rules: [
+                { required: true, message: "Please input your current street!" }
+              ]
+            })(<Input placeholder="Street Address" />)}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator("zipfrom", {
+              rules: [
+                {
+                  required: true,
+                  message: "Please input your current Zip Code!"
+                }
+              ]
+            })(<Input placeholder="Zip Code" />)}
+          </Form.Item>
+          Moving To:
+          <Form.Item>
+            {getFieldDecorator("streetto", {
+              rules: [
+                { required: true, message: "Please input your future street!" }
+              ]
+            })(<Input placeholder="Street Address" />)}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator("zipto", {
+              rules: [
+                {
+                  required: true,
+                  message: "Please input your future Zip Code!"
+                }
+              ]
+            })(<Input placeholder="Zip Code" />)}
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    );
+  }
+}
+
+export default Form.create()(Moving);
